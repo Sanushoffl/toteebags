4a6faad20578594c8415a1864e592f514f8a5b83
Sanushoffl <sanushoffl@gmail.com>
2025-06-09 09:30:37 +0530

    Updated website content and image for tote bags

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -9,7 +9,8 @@ const productSchema = new mongoose.Schema({
     subCategory: { type: String, required: true },
     sizes: { type: Array, required: true },
     bestseller: { type: Boolean },
-    date: { type: Number, required: true }
+    date: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
 })
 
 const productModel  = mongoose.models.product || mongoose.model("product",productSchema);
